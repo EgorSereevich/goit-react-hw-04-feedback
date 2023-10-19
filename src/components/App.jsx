e94f6e7d4efe118e92f03a0ceb3f27a6d66f0f89
Egor Sergeevich <122815068+EgorSereevich@users.noreply.github.com>
2023-10-19 13:08:37 +0300

    19/10 =13:08

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Statistics } from 'components/Statistics/Statistics.jsx';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Headle } from './Headle/Headle';
 import { Notification } from './Notification/Notification';
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  headleClick = state => {
-    this.setState(prevState => {
-      return {
-        [state]: prevState[state] + 1,
-      };
-    });
+const App = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const options = Object.keys(feedback);
+  console.log(feedback);
+  const headleClick = options => {
+    console.log(feedback);
+    setFeedback(prevState => ({
+      ...prevState,
+      [options]: prevState[options] + 1,
+    }));
   };
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    return Math.round((good / this.countTotalFeedback()) * 100);
-  }
-  render() {
-    const options = Object.keys(this.state);
-    return (
-      <Headle>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.headleClick}
+  const countTotalFeedback = () => {
+    return feedback.good + feedback.neutral + feedback.bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((feedback.good / countTotalFeedback()) * 100);
+  };
+
+  return (
+    <Headle>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={options} onLeaveFeedback={headleClick} />
+      </Section>
+      <Section title={'Statistics'}>
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title={'Statistics'}>
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification title={'There is no feedback'} />
-          )}
-        </Section>
-      </Headle>
-    );
-  }
-}
+        ) : (
+          <Notification title={'There is no feedback'} />
+        )}
+      </Section>
+    </Headle>
+  );
+};
+
 export { App };
